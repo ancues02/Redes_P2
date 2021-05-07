61a7c4aec189a4570591c41ff7cbc75cdc83f51d
Andres <ancues02@ucm.es>
2021-05-07 12:04:27 +0200

    Empezando a poner cosas en el main

--- P2.1/Ej1.cc
+++ P2.1/Ej1.cc
@@ -5,5 +5,24 @@
 #include <iostream>
 
 int main(int argc, char** argv){
+    if(argc < 2){
+        std::cerr << "Se necesitan mas argumentos\n";
+        return -1;
+    }
+    struct addrinfo hints;
+    struct addrinfo *res;
+
+    memset((void*) &hints, 0, sizeof(struct addrinfo));
+
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    int rc = getaddrinfo(argv[1], nullptr, &hints, &res);
+
+    if(rc != 0){
+        std::cerr << "[getaddrinfo]: " << gai_strerror(rc) << "\n";
+        return -1;
+    }
+
     return 0;
 }
