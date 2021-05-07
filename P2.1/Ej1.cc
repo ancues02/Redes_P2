a6bbaf709cc2f2f9c51fead60767fcab4ccb1bf2
Andres <ancues02@ucm.es>
2021-05-07 12:22:41 +0200

    Funciona relativamente bien el primer ejercicio

--- P2.1/Ej1.cc
+++ P2.1/Ej1.cc
@@ -12,7 +12,7 @@ int main(int argc, char** argv){
     struct addrinfo hints;
     struct addrinfo *res;
 
-    memset((void*) &hints, 0, sizeof(struct addrinfo));
+    memset((void*) &hints, 0, sizeof(addrinfo));
 
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
@@ -22,6 +22,14 @@ int main(int argc, char** argv){
     if(rc != 0){
         std::cerr << "[getaddrinfo]: " << gai_strerror(rc) << "\n";
         return -1;
+    }
+
+    for(auto i = res; i != nullptr; i = i->ai_next){
+        char host[NI_MAXHOST];
+        char serv[NI_MAXSERV];
+        getnameinfo(i->ai_addr, i->ai_addrlen,host, NI_MAXHOST, serv, NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
+        //std::cout <<"Host: " << host << " Port: "<< serv << "\n";        
+        std::cout << host << " "<< serv << "\n";        
     }
 
     return 0;
