9874401e305e6dd0c1c0e382dacb1a3b4d330285
Andres <ancues02@ucm.es>
2021-05-28 18:02:15 +0200

    Haciendo el ejercicio del chat, socket.cc hecho

---
+++ P2.2/practica2.2/replicacion-chat/Socket.cc
@@ -0,0 +1,105 @@
+#include <string.h>
+
+#include "Serializable.h"
+#include "Socket.h"
+
+Socket::Socket(const char * address, const char * port):sd(-1)
+{
+    //Construir un socket de tipo AF_INET y SOCK_DGRAM usando getaddrinfo.
+    //Con el resultado inicializar los miembros sd, sa y sa_len de la clase
+     struct addrinfo hints;
+    struct addrinfo *res;
+    //reservo memoria para un addrinfo con valor 0
+    memset((void*) &hints, 0, sizeof(addrinfo));
+
+    //cojo IPV4 
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;//UDP
+
+    int rc = getaddrinfo(address, port, &hints, &res);
+
+    if(rc != 0){
+        std::cerr << "[getaddrinfo]: " << gai_strerror(rc) << "\n";
+        return ;
+    }
+
+    sd = socket(res->ai_family, res->ai_socktype,0);
+    if(sd ==-1){
+        std::cerr << "[socket]: creacion socket\n";
+        return ;
+    }
+
+    sa=*res->ai_addr;
+    sa_len=res->ai_addrlen;
+
+    freeaddrinfo(res);
+}
+
+int Socket::recv(Serializable &obj, Socket * &sock)
+{
+    struct sockaddr sa;
+    socklen_t sa_len = sizeof(struct sockaddr);
+
+    char buffer[MAX_MESSAGE_SIZE];
+
+    ssize_t bytes = ::recvfrom(sd, buffer, MAX_MESSAGE_SIZE, 0, &sa, &sa_len);
+
+    if ( bytes <= 0 )
+    {
+        return -1;
+    }
+
+    if ( sock != 0 )
+    {
+        sock = new Socket(&sa, sa_len);
+    }
+
+    obj.from_bin(buffer);
+
+    return 0;
+}
+
+int Socket::send(Serializable& obj, const Socket& sock)
+{
+    //Serializar el objeto
+    //Enviar el objeto binario a sock usando el socket sd
+    obj.to_bin();
+    
+
+    int send = sendto(sock.sd, obj.data(), obj.size(), 0, &sock.sa, sock.sa_len);
+    if(send == -1){
+        std::cerr << "[sendto]: error al mandar el mensaje\n";
+        return -1;
+    }
+
+    return 0;
+}
+
+bool operator== (const Socket &s1, const Socket &s2)
+{
+    //Comparar los campos sin_family, sin_addr.s_addr y sin_port
+    //de la estructura sockaddr_in de los Sockets s1 y s2
+    //Retornar false si alguno difiere
+
+    struct sockaddr_in *s1_in = (struct sockaddr_in* ) &s1.sa;
+    struct sockaddr_in *s2_in = (struct sockaddr_in* ) &s2.sa;
+
+    return (s1_in->sin_family == s2_in->sin_family &&
+    s1_in->sin_addr.s_addr == s2_in->sin_addr.s_addr && 
+    s1_in->sin_port == s2_in->sin_port);
+
+};
+
+std::ostream& operator<<(std::ostream& os, const Socket& s)
+{
+    char host[NI_MAXHOST];
+    char serv[NI_MAXSERV];
+
+    getnameinfo((struct sockaddr *) &(s.sa), s.sa_len, host, NI_MAXHOST, serv,
+                NI_MAXSERV, NI_NUMERICHOST);
+
+    os << host << ":" << serv;
+
+    return os;
+};
+
